47131655697a2922d0a8f48293acc21c324adca9
Roy Bakker <s1161988@student.hsleiden.nl>
2026-03-17 14:06:39 +0100

    Week 5 dagdeel 1

--- src/main/java/org/example/zaalschoenenwebshop/config/SecurityConfig.java
+++ src/main/java/org/example/zaalschoenenwebshop/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/zaalschoenen/**").permitAll()
                         .requestMatchers("/error").anonymous()
                         .anyRequest().authenticated()
                 )
--- src/main/java/org/example/zaalschoenenwebshop/utils/Seeder.java
+++ src/main/java/org/example/zaalschoenenwebshop/utils/Seeder.java
@@ -2,28 +2,36 @@ package org.example.zaalschoenenwebshop.utils;
 
 
 import org.example.zaalschoenenwebshop.dao.CategoryRepository;
+import org.example.zaalschoenenwebshop.dao.UserRepository;
 import org.example.zaalschoenenwebshop.dao.ZaalSchoenRepository;
 import org.example.zaalschoenenwebshop.models.Category;
+import org.example.zaalschoenenwebshop.models.CustomUser;
 import org.example.zaalschoenenwebshop.models.ZaalSchoen;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Seeder {
 
+    private final PasswordEncoder passwordEncoder;
     private ZaalSchoenRepository zaalSchoenRepository;
     private CategoryRepository categoryRepository;
+    private UserRepository userRepository;
 
-    public Seeder(ZaalSchoenRepository zaalSchoenRepository, CategoryRepository categoryRepository) {
+    public Seeder(ZaalSchoenRepository zaalSchoenRepository, CategoryRepository categoryRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.zaalSchoenRepository = zaalSchoenRepository;
         this.categoryRepository = categoryRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @EventListener
     public void seed(ContextRefreshedEvent event){
         this.seedZaalSchoenen();
     }
+
 
     private void seedZaalSchoenen(){
         Category basketbalSchoenen = new Category("Basketbal Schoenen");
@@ -32,5 +40,14 @@ public class Seeder {
         this.zaalSchoenRepository.save(new ZaalSchoen("Gel-court Hunter 3", "Zaalschoen", basketbalSchoenen, "Mizuno", 149.99));
         this.zaalSchoenRepository.save(new ZaalSchoen("Wave Momentum 3", "ZaalSchoen", basketbalSchoenen, "Mizuno", 85.00));
         this.zaalSchoenRepository.save(new ZaalSchoen("Wave Luminmous 3", "Zaalschoen", basketbalSchoenen, "Mizuno", 105.00));
+        this.zaalSchoenRepository.save(new ZaalSchoen("Hyperice Hyperboot", "HyperIce", basketbalSchoenen, "Nike", 749.99));
+
+
+        this.userRepository.save(new CustomUser(
+                "test14321@gmail.com",
+                passwordEncoder.encode("Test123.")
+        ));
     }
+
+
 }
